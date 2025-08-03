cf5d0c88fe58267f49402950d495998be129cc3c
Abdellah saim mamoune <abdellahsaimmamoune1@gmail.com>
2025-08-03 22:32:44 +0200

    deleting business layer

--- Backend/BusinessLayer/Dto's/EmployeeDto's/DGetResentTransactions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace bankApI.BusinessLayer.Dto_s.EmployeeDto_s
-{
-    public class DGetResentTransactions
-    {
-        public List<DGetAllClientsTransactionsHistory>? TransactionsHistory { get; set; } = new();
-        public List<DGetTransferFundHistory>? TransferFundHistory { get; set; } = new();
-    }
-}
--- Backend/BusinessLayer/Dto's/EmployeeDto's/DSendMessageToClient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace bankApI.BusinessLayer.Dto_s.EmployeeDto_s
-{
-    public class DSendMessageToClient
-    {
-        public string AccountId { get; set; }
-        public string Message { get; set; }
-    }
-}
